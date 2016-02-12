5b165e832919a2e1e43780c183a37963a8931067
t10471 <t104711202@gmail.com>
2016-02-12 20:25:56 +0900

    added init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ cp /root/workspace/tmp/lushtags/dist/build/lushtags/lushtags /usr/bin/.
 cp /root/workspace/tmp/hothasktags/dist/build/hothasktags/hothasktags /usr/bin/.
 cp /root/workspace/tmp/cpphs-1.19/dist/build/cpphs/cpphs /root/.cabal/bin/.
 cp /root/workspace/tmp/pointfree-1.1/dist/build/pointfree/pointfree /root/.cabal/bin/.
+#git clone git@github.com:kazu-yamamoto/ghc-mod.git
+#cd ghc-mod/
+#cabal sandbox init
+cp /root/workspace/tmp/ghc-mod/dist/build/ghc-mod /root/.cabal/bin/.
+cp /root/workspace/tmp/ghc-mod/dist/build/ghc-modi/ghc-modi /root/.cabal/bin/.
+
